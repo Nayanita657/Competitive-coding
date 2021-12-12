4fa24921b330a225f1a799a87f905e1fab652481
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-11 22:22:12 -0800

    Finding Duplicate in Array - Time Complexity -O(n)
    You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3, and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.
    
    Constraints:-
    1 <= t <= 10^2
    0 <= N <= 10^6
    
    Time Limit: 1 sec

---
+++ Array/Duplicate in Array.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+
+
+int findDuplicate(int *arr, int n)
+{
+   //creating an vector and copying the element from array to vector
+    vector<int> cal_array(arr + 0, arr + n);
+    //unordered map holds the occurance of the element in the list
+    unordered_map<long long, long long> umap;
+    //pushing element in the unordered list from vector
+    for(auto x:cal_array)
+        umap[x]++;
+    
+    int element;
+    for (auto itr = umap.begin(); itr != umap.end(); itr++) 
+    {
+        if(itr->second == 2)//finding the duplicate element
+        {
+            element=itr->first;
+            break;
+        }
+    }
+    return element;
+}
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << findDuplicate(input, size) << endl;
+	}
+
+	return 0;
+}
