47d1b9bb8a9df2d5b59521fc4b75bfee70346481
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-11 22:12:03 -0800

    Find the Unique Element
    You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
    Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
    You need to find and return that number which is unique in the array/list
    
    Constraints:
    1 <= t <= 10^2
    0 <= N <= 10^6
    
    Time Limit: 1 sec

---
+++ Array/Find the Unique element.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+
+int findUnique(int *arr, int n) 
+{
+    //creating an vector and copying the element from array to vector
+    vector<int> cal_array(arr + 0, arr + n);
+    //unordered map holds the occurance of the element in the list
+    unordered_map<long long, long long> umap;
+    //pushing element in the unordered list from vector
+    for(auto x:cal_array)
+        umap[x]++;
+    
+    int element;
+    for (auto itr = umap.begin(); itr != umap.end(); itr++) 
+    {
+        if(itr->second == 1) //finding the element whose occurence is 1 that actually indictes unique element
+        {
+            element=itr->first;
+            break;
+        }
+    }
+    return element;
+    
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int size;
+        cin >> size;
+        int *input = new int[size];
+
+        for (int i = 0; i < size; ++i) {
+            cin >> input[i];
+        }
+
+        cout << findUnique(input, size) << endl;
+    }
+
+    return 0;
+}
