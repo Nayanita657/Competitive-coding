24cf6c599496e581b4228b87a1135a001c15ec0b
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-12 23:17:07 -0800

    Kth smallest element using set
    Given an array and a number k where k is smaller than the size of the array, we need to find the k’th smallest element in the given array. It is given that all array elements are distinct.

---
+++ Array/Kth smallest element using set.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int GetKthsmallest(int *arr, int size, int K)
+{
+    //int size = r - l + 1;
+    set<int> c(arr,arr+size);
+    
+    set<int>::iterator itr;
+    itr = c.begin();
+    advance(itr,K-1);
+    
+    return *itr;
+}
+
+int main()
+{
+    int no_testcase;
+    cout<<"Enter no of tets cases:\n";
+    cin>>no_testcase;
+    
+    int size;
+    cout<<"Enter size\n";
+    cin>>size;
+    int *arr = new int(size);
+    
+    cout<<"Enter elements:\n";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Enter K value-\n";
+    int K;
+    cin>>K;
+    
+    int result = GetKthsmallest(arr,size,K);
+    cout<<result;
+}
