3aa4f854735c18ddaad674517361e7383647261c
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-13 05:19:01 -0800

    Move all negative numbers at one end using two pointer approach Tc - O(n)
    An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.

---
+++ Array/Move all negative numbers at on end using two pointer approach.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void rearrange(int *a,int left_ptr,int right_ptr)
+{
+    while(left_ptr < right_ptr)
+    {
+        if(a[left_ptr] < 0 && a[right_ptr] < 0)
+        {
+            left_ptr++;
+        }
+        else if(a[left_ptr] > 0 && a[right_ptr] > 0)
+        {
+            right_ptr--;
+        }
+        else if(a[left_ptr] > 0 && a[right_ptr] < 0)
+        {
+            //swap
+            swap(a[left_ptr],a[right_ptr]);
+            left_ptr--;
+            right_ptr++;
+        }
+        else
+        {
+            left_ptr++;
+            right_ptr--;
+        }
+    }
+}
+
+void printArray(int *arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main()
+{
+    int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rearrange(arr, 0,n-1);
+    printArray(arr, n);
+    return 0;
+}
