98077003898f4b43366dde4c8695141a35e58868
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-18 19:59:41 -0800

    Inserting data in linked list

---
+++ Linked List/Inserting Data in Linked list.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node *next;
+  
+  
+  Node(int data)
+  {
+      this->data = data;
+      next = NULL;
+  }
+};
+
+Node *Takeinput()
+{
+    int data;
+    cout<<"Enter data:";
+    cin>>data;
+    
+    Node *head = NULL;
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while(temp -> next != NULL)
+            {
+                temp = temp -> next;
+            }
+            temp->next = newNode;
+        }
+        cout<<"\nEnter data:";
+        cin>>data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data <<" ";
+        temp = temp->next;
+    }
+}
+
+
+int main()
+{
+    Node *head = Takeinput();
+    print(head);
+    
+    return 0;
+}
