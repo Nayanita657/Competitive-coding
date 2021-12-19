aa5150a1f9d0fee0fc05825d9b99cffd0e219778
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-19 04:03:02 -0800

    Reverse Linked list-Iterative approach

---
+++ Linked List/Reverse Linked list-Iterative approach.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *insertNode()
+{
+    int data;
+    cout<<"\nEnter data:";
+    cin>>data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cout<<"\nEnter data:";
+        cin>>data;
+        
+    }
+    return head;
+    
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+Node *reverLL(Node *head)
+{
+    Node *prev = NULL;
+    Node *next = NULL;
+    Node *curr = head;
+    
+    Node *temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    
+    while(curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return temp;
+}
+
+int main()
+{
+    Node *head = insertNode();
+    print(head);
+    cout<<"\nReversing linked list print:\n";
+    head = reverLL(head);
+    print(head);
+}
