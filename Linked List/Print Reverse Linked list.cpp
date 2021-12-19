aebcb2e26157a715b80f7b0d8ab5f753a1d33e74
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-19 02:26:54 -0800

    Print elements in LL in reverse order. TC-O(n)
    You have been given a singly linked list of integers. Write a function to print the list in a reverse order.
    To explain it further, you need to start printing the data from the tail and move towards the head of the list, printing the head data at the end.
    Note :
    You can’t change any of the pointers in the linked list, just print it in the reverse order.
    Remember/Constraints :
    While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.

---
+++ Linked List/Print Reverse Linked list.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+void printReverse(Node *head)
+{
+   if(head != NULL)
+   {
+       if(head->next == NULL)
+   	   {
+       	cout<<head->data<<" ";
+   		}
+    	else
+    	{
+        	Node *temp = head;
+        	head = head->next;
+        	printReverse(head);
+        	cout<<temp->data<<" ";
+    	}
+   }
+   
+    
+   
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		printReverse(head);
+		cout << endl;
+	}
+	return 0;
+}
