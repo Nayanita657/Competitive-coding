9868101c4f80c6acf7d2d577413ef26409eded8b
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-25 01:17:38 -0800

    Stack using Template

---
+++ Stack./Stack using Template.cpp
@@ -0,0 +1,105 @@
+
+
+#include <iostream>
+
+using namespace std;
+#include <bits/stdc++.h>
+template <typename T>
+
+class stackUsingArray{
+    T *data;
+    int nextIndex;
+    int capacity;
+    
+    public:
+    stackUsingArray(int size)
+    {
+        data = new T[size];
+        nextIndex = 0;
+        capacity = size;
+    }
+    
+    T top()
+    {
+        if(isEmpty())
+        {
+            cout<<"Stack empty \n";
+            return 0;
+        }
+        return data[nextIndex - 1];
+    }
+    
+    int size()
+    {
+        return nextIndex;
+    }
+    
+    bool isEmpty()
+    {
+        if(nextIndex == 0)
+            return true;
+        else
+            return false;
+    }
+    
+    void push(T element)
+    {
+        if(nextIndex == capacity)
+        {
+            cout<<"stack full \n";
+            
+        }
+        else
+        {
+            data[nextIndex] = element;
+            nextIndex++;
+        }
+        
+    }
+    
+    T pop()
+    {
+        if(isEmpty())
+        {
+            cout<<"stack empty \n";
+            return 0;
+        }
+        else
+        {
+            nextIndex--;
+            return data[nextIndex];
+        }
+    }
+    
+    void display()
+    {
+        for(int i=0; i<nextIndex ; i++)
+        {
+            cout<<data[i]<<" ";
+        }
+        cout<<"\n";
+    }
+};
+
+int main()
+{
+    stackUsingArray<char> s1(4);
+    
+    s1.push(65);
+    s1.push(66);
+    s1.push(67);
+    s1.push(68);
+    s1.push(69);
+    
+    s1.display();
+    
+    cout<<" top element : "<<s1.top()<<endl;
+    cout<<"popped element : "<<s1.pop()<<endl;
+    cout<<"popped element : "<<s1.pop()<<endl;
+    cout<<"popped element : "<<s1.pop()<<endl;
+    cout<<"popped element : "<<s1.pop()<<endl;
+    cout<<"popped element : "<<s1.pop()<<endl;
+    
+    return 0;
+}
+
