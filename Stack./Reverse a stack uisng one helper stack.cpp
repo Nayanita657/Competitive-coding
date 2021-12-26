14fb50029602f39db591cac14520066ea20a4e9e
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-25 17:14:05 -0800

    Reverse stack using one helper stack

---
+++ Stack./Reverse a stack uisng one helper stack.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+
+void reverseStack(stack<int> &stack1, stack<int> &stack2) 
+{
+
+    if(stack1.size() == 1 || stack1.empty())
+    {
+        return ;
+    }
+    else
+    {
+        int item = stack1.top();
+        stack1.pop();
+        reverseStack(stack1,stack2);
+        //move the elements from stack to extra
+        while(!stack1.empty())
+        {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+        //place the item in the std::stack<
+        stack1.push(item);
+        while(!stack2.empty())
+        {
+            stack1.push(stack2.top());
+            stack2.pop();
+        }
+        
+    }
+
+    
+    
+}
+
+int main() {
+    stack<int> input, extra;
+    int size;
+    cin >> size;
+
+    for (int i = 0, val; i < size; i++) {
+        cin >> val;
+        input.push(val);
+    }
+
+    reverseStack(input, extra);
+
+    while (!input.empty()) {
+        cout << input.top() << " ";
+        input.pop();
+    }
+}
