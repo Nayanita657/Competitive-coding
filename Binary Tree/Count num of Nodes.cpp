e205216c7b4c149c476a865e98dd836355149b84
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2021-12-31 06:03:20 -0800

    Count num of Nodes

---
+++ Binary Tree/Count num of Nodes.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class BinaryTreeNode{
+    public:
+    int data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+    
+    BinaryTreeNode(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        
+    }
+    ~BinaryTreeNode()
+    {
+        delete left;
+        delete right;
+    }
+};
+
+void printTree(BinaryTreeNode *root)
+{
+    //base case
+    if(root == NULL)
+        return;
+    cout<<root->data<<":";
+    if(root->left != NULL)
+    {
+        cout<<"L"<<root->left->data<<" ";
+    }
+    if(root->right != NULL)
+    {
+        cout<<"R"<<root->right->data;
+    }
+    cout<<endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+int countNodes(BinaryTreeNode* root)
+{
+    if(root == NULL)
+        return 0;
+    else
+    {
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }    
+}
+
+BinaryTreeNode *takeInputLevelwise()
+{
+    int rootData;
+    cout<<"Enter root data : ";
+    cin>>rootData;
+    
+    if(rootData == -1)
+    {
+        return NULL;
+    }
+    else
+    {
+        BinaryTreeNode *root = new BinaryTreeNode(rootData);
+        queue<BinaryTreeNode *> pendingNodes;
+        pendingNodes.push(root);
+        while(!pendingNodes.empty())
+        {
+            BinaryTreeNode *front = pendingNodes.front();
+            pendingNodes.pop();
+            
+            cout<<"Enter left child of "<<front->data <<" : ";
+            int leftChildData;
+            cin>>leftChildData;
+            if(leftChildData != -1)
+            {
+                BinaryTreeNode *child = new BinaryTreeNode(leftChildData);
+                front->left = child;
+                pendingNodes.push(child);
+            }
+            cout<<"Enter right child of "<<front->data<<" : ";
+            int rightChildData;
+            cin>>rightChildData;
+            if(rightChildData != -1)
+            {
+                BinaryTreeNode *child = new BinaryTreeNode(rightChildData);
+                front->right = child;
+                pendingNodes.push(child);
+            }
+            
+        }
+        return root;
+    }
+}
+
+int main()
+{
+   // BinaryTreeNode *root = takeInput();
+    BinaryTreeNode *root = takeInputLevelwise();
+    printTree(root);
+    cout<<endl;
+    cout<<"num of Nodes : "<<countNodes(root);
+
+    return 0;
+}
