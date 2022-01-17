7eb742fab92430597f79a1c8b65b10449bc857ec
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-01-17 14:43:35 +0530

    Inbuilt Min Priority Queue

---
+++ Priority Queue/Inbuilt Min Priority Queue.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(100);
+    pq.push(21);
+    pq.push(7);
+    pq.push(165);
+    pq.push(75);
+    pq.push(4);
+    
+    cout<<pq.size()<<endl;
+    cout<<pq.empty()<<endl;
+    cout<<pq.top()<<endl;
+    
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    
+    return 0;
+}
