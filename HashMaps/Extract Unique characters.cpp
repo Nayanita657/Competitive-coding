fea6e6adc92bade7a41c851f961d4904bdd11a8d
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-01-22 13:08:08 +0530

    Extract Unique characters
    Given a string S, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same, as in the input string.
    Input format:
    The first and only line of input contains a string, that denotes the value of S.
    Output format :
    The first and only line of output contains the updated string, as described in the task.
    Constraints :
    0 <= Length of S <= 10^8
    Time Limit: 1 sec
    Sample Input 1 :
    ababacd
    Sample Output 1 :
    abcd
    Sample Input 2 :
    abcde
    Sample Output 2 :
    abcde

---
+++ HashMaps/Extract Unique characters.cpp
@@ -0,0 +1,31 @@
+#include <unordered_map>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string uniqueChar(string str) 
+{
+	string output;
+    unordered_map<char,int> umap;
+    
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(umap.find(str[i]) != umap.end())
+        {
+           continue;
+        }
+        else
+        {
+            char temp = str[i];
+            output += temp; 
+            umap[str[i]]++;
+        }
+    }
+    return output;
+}
+int main() 
+{
+    string str;
+    cin >> str;
+    cout << uniqueChar(str);
+}
