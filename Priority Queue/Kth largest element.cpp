fbc5e84e66c547c760b44ee919745fbb5537383b
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-01-25 08:40:51 +0530

    Kth largest element Tc -O(N) Sc-O(1)
    Given an array A of random integers and an integer k, find and return the kth largest element in the array.
    Note: Try to do this question in less than O(N * logN) time.
    Input Format :
    The first line of input contains an integer, that denotes the value of the size of the array. Let us denote it with the symbol N.
    The following line contains N space separated integers, that denote the value of the elements of the array.
    The following contains an integer, that denotes the value of k.
    Output Format :
    The first and only line of output contains the kth largest element
    Constraints :
    1 <= N, Ai, k <= 10^5
    Time Limit: 1 sec
    Sample Input 1 :
    6
    9 4 8 7 11 3
    2
    Sample Output 1 :
    9
    Sample Input 2 :
    8
    2 6 10 11 13 4 1 20
    4
    Sample Output 2 :
    10

---
+++ Priority Queue/Kth largest element.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <queue>
+int kthLargest(int* arr, int n, int k) 
+{
+    // Write your code here
+    priority_queue<int> pq;
+ 	for(int i = 0; i < n; i++)
+    {
+        pq.push(arr[i]);
+    }
+    for(int i = 1; i < k ; i++)
+    {
+        pq.pop();
+    }
+    return pq.top();
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cin >> k;
+
+    cout << kthLargest(arr, n, k);
+
+    delete[] arr;
+}
